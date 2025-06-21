3e583dc0f06112c23b7bd2f3739342b33a9af6db
Maciej Kędra <maciejkedra@Mac-mini-Maciej.local>
2025-06-21 14:47:37 +0200

    update puk for mac

--- keyboards/keychron/q1v1/ansi/keymaps/maciejken/keymap.c
+++ keyboards/keychron/q1v1/ansi/keymaps/maciejken/keymap.c
@@ -24,26 +24,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Mac
     [0] = LAYOUT_ansi_82(
         KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,  KC_INS,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_LBRC,  KC_RBRC,  KC_BSPC,           KC_PGUP,
-        KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,  KC_EQL,   KC_BSLS,           KC_PGDN,
-        KC_CAPS,  KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,            KC_ENT,            KC_HOME,
-        KC_LSFT,            KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,               KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LCMD,  KC_LOPT,                                KC_SPC,                                 MO(1),    KC_ROPT,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
+        KC_GRV,   KC_LPRN,  KC_X,     PL_SACU,  KC_F,     PL_ZACU,  KC_Q,     PL_ZDOT,  PL_CACU,  PL_NACU,  KC_V,     KC_LBRC,  KC_RBRC,  KC_BSPC,           KC_PGUP,
+        KC_TAB,   KC_P,     KC_COMM,  KC_DOT,   KC_U,     KC_B,     KC_J,     KC_T,     KC_C,     KC_K,     KC_D,     KC_SLSH,  KC_EQL,   KC_BSLS,           KC_PGDN,
+        KC_CAPS,  KC_Y,     KC_I,     KC_A,     KC_E,     KC_O,     KC_W,     KC_Z,     KC_R,     KC_N,     KC_S,     KC_MINS,            KC_ENT,            KC_HOME,
+        KC_LSFT,            KC_SCLN,  KC_H,     PL_AOGO,  PL_EOGO,  PL_OACU,  PL_LSTR,  KC_M,     KC_L,     KC_G,     KC_QUOT,            KC_RSFT,  KC_UP,
+        KC_LCTL,  MO(1),    KC_LGUI,                                KC_SPC,                                 KC_RGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
 
     // Mac_fn
     [1] = LAYOUT_ansi_82(
         QK_BOOT,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_SLEP, RM_SATU,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           RM_HUEU,
-        RM_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           RM_HUED,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,           RM_SATD,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  RM_SPDU,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_RPRN,  _______,  _______,  _______,           RM_HUEU,
+        RM_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_PERC,  _______,  _______,           RM_HUED,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               _______,           RM_SATD,
+        _______,            _______,  _______,  _______,  _______,  _______,  KC_EXLM,  KC_DOT,   KC_AT,    KC_HASH,  KC_DLR,             _______,  RM_SPDU,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  RM_PREV,  RM_SPDD, RM_NEXT),
 
     // Win
     [2] = LAYOUT_ansi_82(
         KC_ESC,             KC_BRID,  KC_BRIU,  KC_PSCR,  KC_CALC,  RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,  KC_INS,
-        KC_GRV,   KC_EXLM,  KC_X,     PL_SACU,  KC_F,     PL_ZACU,  KC_Q,     PL_ZDOT,  PL_CACU,  PL_NACU,  KC_V,     KC_LBRC,  KC_RBRC,  KC_BSPC,           KC_PGUP,
-        KC_TAB,   KC_P,     KC_COMM,  KC_DOT,   KC_U,     KC_B,     KC_J,     KC_T,     KC_C,     KC_K,     KC_D,     KC_QUES,  KC_EQL,   KC_BSLS,           KC_PGDN,
+        KC_GRV,   KC_LPRN,  KC_X,     PL_SACU,  KC_F,     PL_ZACU,  KC_Q,     PL_ZDOT,  PL_CACU,  PL_NACU,  KC_V,     KC_LBRC,  KC_RBRC,  KC_BSPC,           KC_PGUP,
+        KC_TAB,   KC_P,     KC_COMM,  KC_DOT,   KC_U,     KC_B,     KC_J,     KC_T,     KC_C,     KC_K,     KC_D,     KC_SLSH,  KC_EQL,   KC_BSLS,           KC_PGDN,
         KC_CAPS,  KC_Y,     KC_I,     KC_A,     KC_E,     KC_O,     KC_W,     KC_Z,     KC_R,     KC_N,     KC_S,     KC_MINS,            KC_ENT,            KC_HOME,
         KC_LSFT,            KC_SCLN,  KC_H,     PL_AOGO,  PL_EOGO,  PL_OACU,  PL_LSTR,  KC_M,     KC_L,     KC_G,     KC_QUOT,            KC_RSFT,  KC_UP,
         KC_LCTL,  MO(3),    KC_LALT,                                KC_SPC,                                 TG(3),    KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
@@ -51,10 +51,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Win_fn
     [3] = LAYOUT_ansi_82(
         KC_PWR,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_SLEP, RM_SATU,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_7,     KC_8,     KC_9,     KC_0,     _______,  _______,  _______,           RM_HUEU,
-        RM_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_4,     KC_5,     KC_6,     KC_ASTR,  KC_SLSH,  _______,  _______,           RM_HUED,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_1,     KC_2,     KC_3,     KC_PLUS,  _______,            _______,           RM_SATD,
-        _______,            _______,  _______,  _______,  _______,  _______,  KC_LPRN,  KC_RPRN,  _______,  _______,  _______,            _______,  RM_SPDU,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_RPRN,  _______,  _______,  _______,           RM_HUEU,
+        RM_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_PERC,  _______,  _______,           RM_HUED,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               _______,           RM_SATD,
+        _______,            _______,  _______,  _______,  _______,  _______,  KC_EXLM,  KC_DOT,   KC_AT,    KC_HASH,  KC_DLR,             _______,  RM_SPDU,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  RM_PREV,  RM_SPDD, RM_NEXT),
     
     // Win_dvorak
