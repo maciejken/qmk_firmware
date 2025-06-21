e8e31afbf55af608f2d8b4f2e3dbeaa2b885094f
Maciej Kędra <maciejken@gmail.com>
2025-06-21 15:22:39 +0200

    remove sleep code from mac layout

--- keyboards/keychron/q1v1/ansi/keymaps/maciejken/keymap.c
+++ keyboards/keychron/q1v1/ansi/keymaps/maciejken/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // Mac_fn
     [1] = LAYOUT_ansi_82(
-        QK_BOOT,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_SLEP, RM_SATU,
+        QK_BOOT,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______, RM_SATU,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_RPRN,  _______,  _______,  _______,           RM_HUEU,
         RM_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,  KC_6,     KC_7,     KC_8,     KC_9,     KC_PERC,  _______,  _______,           RM_HUED,
         _______,  _______,  _______,  _______,  _______,  _______,  KC_0,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               _______,           RM_SATD,
